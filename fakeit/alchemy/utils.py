cd4a7c37380f9ccb0c32de8fa6ee0d814a520c01
Ruslan Samoylov <ruslan.v.samoylov@gmail.com>
2019-02-05 03:15:36 +0300

    partial fixes

--- fakeit/alchemy/utils.py
+++ fakeit/alchemy/utils.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from fakeit.basics.numerics import fake_int, fake_float
 from fakeit.basics.strings import fake_string
 
@@ -23,3 +25,26 @@ type_mapping = {
     int: fake_int,
     float: fake_float
 }
+
+
+def fill_template(template, id=None):
+    tmpl = deepcopy(template)
+    res = {}
+    if "id" in tmpl:
+        tmpl.pop("id")
+        if id is None:
+            id = fake_int(0, 128)
+
+    for k, v in tmpl.items():
+        if v.python_type == int:
+            res[k] = type_mapping[int](0, 777)
+        elif v.python_type == float:
+            res[k] = type_mapping[float](0, 777)
+        elif v.python_type == str:
+            res[k] = type_mapping[str](max_length=v.length)
+        else:
+            res[k] = None
+
+    if ("id" in template) or id:
+        res["id"] = id
+    return res
